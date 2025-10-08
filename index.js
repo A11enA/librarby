aae20cfbf367ff6c3321e0a9a356b5821945d25f
A11enA <alasiakallen@gmail.com>
2025-10-08 00:02:17 +0000

    Adding calculate distance

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 (function(window, _) {
   window.myLibraryName = window.myLibraryName || {
-    numz: {},
+    numz: {
+
+      calculateDistance(A, B){
+      var leg1 = Math.abs(B.x - A.x);
+      var leg2 = Math.abs(B.y - A.y);
+      var uns = (leg1**2) + (leg2**2);
+      var distance = uns ** (1/2);
+      return distance
+    }
+
+    },
     phyz: {},
   };
 }(window, window._));
