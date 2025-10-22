f995594cbad15457b24d66e3acd4d41a82e909a8
A11enA <alasiakallen@gmail.com>
2025-10-22 00:40:13 +0000

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function (window, _) {
-  window.myLibraryName = window.myLibraryName || {
+  window.librarby = window.librarby || {
     numz: {
       calculateDistance(A, B) {
         var leg1 = Math.abs(B.x - A.x);
