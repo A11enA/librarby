b2a43d951748b8e49386992a1386890717813daf
A11enA <alasiakallen@gmail.com>
2025-10-22 00:18:12 +0000

    added degree getter

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-(function(window, _) {
+(function (window, _) {
   window.myLibraryName = window.myLibraryName || {
     numz: {
+      calculateDistance(A, B) {
+        var leg1 = Math.abs(B.x - A.x);
+        var leg2 = Math.abs(B.y - A.y);
+        var uns = leg1 ** 2 + leg2 ** 2;
+        var distance = uns ** (1 / 2);
+        return distance;
+      },
 
-      calculateDistance(A, B){
-      var leg1 = Math.abs(B.x - A.x);
-      var leg2 = Math.abs(B.y - A.y);
-      var uns = (leg1**2) + (leg2**2);
-      var distance = uns ** (1/2);
-      return distance
-    },
+      getAngleDegrees(pointA, pointB) {
+        const distanceX = pointB.x - pointA.x;
+        const distanceY = pointB.y - pointA.y;
+        const radians = Math.atan2(distanceY, distanceX);
+        const degrees = (radians * 180) / Math.PI;
+        return degrees;
+      },
 
-    degreesToRadians(degrees) {
-      return degrees * Math.Pi / 180;
-    },
+      degreesToRadians(degrees) {
+        return (degrees * Math.Pi) / 180;
+      },
 
-    radiansToDegrees(Radians) {
-      return radians * 180/Math.PI;
-    }
-
+      radiansToDegrees(radians) {
+        return (radians * 180) / Math.PI;
+      },
     },
     phyz: {},
   };
-}(window, window._));
+})(window, window._);
