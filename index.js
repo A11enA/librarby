8eb1d82435834bec0ae32cb1e64b5f3b5af5eda2
A11enA <alasiakallen@gmail.com>
2025-10-29 00:15:19 +0000

    adding velocity, position, and makebody

--- index.js
+++ index.js
@@ -25,6 +25,44 @@
         return (radians * 180) / Math.PI;
       },
     },
-    phyz: {},
+    phyz: {
+      makeBody: function (
+        type,
+        {
+          velocityX = 0,
+          velocityY = 0,
+          rotationalVelocity = 0,
+          integrity = 1,
+          density = 1,
+          volatility = 0,
+        } = {}
+      ) {
+        if (type === undefined)
+          throw new Error(
+            "You must provide a valid String for the type parameter!"
+          );
+        return {
+          type: type,
+          velocityX: velocityX,
+          velocityY: velocityY,
+          rotationalVelocity: rotationalVelocity,
+          integrity: integrity,
+          density: density,
+          volatility: volatility,
+        };
+      },
+      updateVelocity(body, forceOnX, forceOnY) {
+        const angle = (body.rotation * Math.PI) / 180,
+          accelerationOnX = Math.cos(angle) * forceOnX,
+          accelerationOnY = Math.sin(angle) * forceOnY;
+        body.velocityX += accelerationOnX;
+        body.velocityY += accelerationOnY;
+      },
+      updatePosition(body) {
+        body.x += body.velocityX;
+        body.y += body.velocityY;
+        body.rotation += body.rotationalVelocity;
+      },
+    },
   };
 })(window, window._);
